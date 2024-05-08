2b9544fd5d7de42da5c46418866ec80bafa94466
Tim Neumann <87799756+timneumann1@users.noreply.github.com>
2024-05-07 23:23:13 -0400

    Added timeout to command in sandbox.py

--- funsearch/sandbox.py
+++ funsearch/sandbox.py
@@ -87,7 +87,10 @@ class ExternalProcessSandbox(DummySandbox):
     output_file = call_data_path / "output.pickle"
     cmd = (f"{self.python_path} {CONTAINER_MAIN} {prog_path} {input_path} {output_file}"
            f"  2> {error_file_path}")
+    cmd = f"timeout {self.timeout_secs} {cmd}"
+    
     logging.debug(f"Executing: {cmd}")
+    
     return os.system(cmd)
 
   def run(
